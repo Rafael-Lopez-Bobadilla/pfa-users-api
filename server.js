f35b5e4b6c142fc0fbaa7424c4c50322c7616471
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-02-02 19:42:07 +0100

    set up done

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
+const app = require('./app')
+
+const DB = process.env.DATABASE.replace('<password>',
+  process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB).then(conection => {
+  console.log('database conected')
+})
+
+const server = app.listen(5002, () => console.log('app running'))
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message)
+  console.log('Shutting Down')
+  server.close(() => {
+    process.exit(1)
+  })
+})
