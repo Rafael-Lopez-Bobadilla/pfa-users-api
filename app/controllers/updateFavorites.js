7e27340b80a2ba2b27f373299c662a04a01b8bc6
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-19 15:51:48 +0100

    modified updateFavorites function

--- app/controllers/updateFavorites.js
+++ app/controllers/updateFavorites.js
@@ -1,15 +1,23 @@
 const User = require('../userModel')
 const { createError } = require('../utils/createError')
-
+const { getResUser } = require('../utils/getResUser')
 exports.updateFavorites = async (req, res) => {
+  const user = req.user
+  let { favorites } = user
+  if (favorites.includes(req.body.favorite)) {
+    favorites.splice(favorites.indexOf(req.body.favorite), 1)
+  } else {
+    favorites.push(req.body.favorite)
+  }
   try {
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, { favorites: req.body.favorites }, {
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, { favorites: favorites }, {
       new: true,
       runValidators: true
     })
+    const resUser = getResUser(updatedUser)
     res.status(200).json({
       status: 'Success',
-      user: updatedUser
+      user: resUser
     })
   } catch (err) {
     createError(res, 'Something went wrong', 400)
