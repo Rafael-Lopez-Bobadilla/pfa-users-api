2e1ba0bc4708406b3366e9057ed78e778be839a4
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-22 12:14:50 +0100

    some

--- app/app.js
+++ app/app.js
@@ -6,10 +6,10 @@ const router = require('./routes')
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 app.use(cors({
-  origin: true,
+  origin: 'http://localhost:5173',
   credentials: true
 }))
-//app.use(helmet())
+app.use(helmet())
 app.use(express.json())
 app.use('/api/v1', router)
 module.exports = app
