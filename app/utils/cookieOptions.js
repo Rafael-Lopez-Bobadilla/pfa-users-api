2db03e0971457cee4aa37e5c915774ee86c83b02
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-06-07 19:28:31 +0200

    set cookie to sameSite strict

--- app/utils/cookieOptions.js
+++ app/utils/cookieOptions.js
@@ -1,15 +1,12 @@
 exports.cookieOptions = (logout) => {
   const options = {
-    expires: new Date(Date.now() +
-      process.env.COOKIE_EXPIRES
-      * 1000 /*milisegundos a segundos*/
-      * 60 /*segundos a minutos*/
-      * 60 /*minutos a horas*/),
-    secure: true, //only sent over https
-    httpOnly: true, //this way it cant be manipulated by javascript on the client
-  }
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 1000 * 60 * 60),
+    secure: true,
+    httpOnly: true,
+    sameSite: "Strict",
+  };
   if (logout) {
-    options.expires = new Date(Date.now())
+    options.expires = new Date(Date.now());
   }
-  return options
-}
+  return options;
+};
