d6665f3797d2b6780519a8cd656790a54bfaf88e
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-07-18 10:50:06 +0200

    consolidated expire env variable. Added token refresh on authenticated controller

--- app/controllers/verifyJwt.js
+++ app/controllers/verifyJwt.js
@@ -9,13 +9,6 @@ exports.verifyJwt = async (req, res, next) => {
       throw createError("Not logged in", 401);
     }
     const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-    const expDate = decoded.exp * 1000; //decoded.exp = seconds since epoch
-    const currentDate = Date.now(); // milliseconds since epoch
-    const minutesLeft = new Date(expDate - currentDate) / (1000 * 60);
-    if (minutesLeft < 15) {
-      const newToken = getToken(decoded.id);
-      res.cookie("pfa_jwt", newToken, cookieOptions());
-    }
     req.userID = decoded.id;
     next();
   } catch (err) {
